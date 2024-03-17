13babb6a38ced95a4ee3e01ead3b50c15375adeb
Rebisha Pokharel <rebisha67@gmail.com>
2024-03-17 22:58:53 +1100

    Add font

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const App = () => (
-    <h1 className="text-4xl bg-black text-opacity-30 text-amber-400">Typescript, Tailwind and Webpack starter</h1>
+    <h1 className="text-4xl text-amber-400 font-display">Typescript, Tailwind and Webpack starter</h1>
 )
 
 export default App
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,10 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx,html,js}"],
   theme: {
+    fontFamily: {
+      'display': ['Roboto', 'sans-serif'],
+      'body': ['Roboto Regular', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
